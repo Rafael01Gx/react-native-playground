1ba746182c78cea8a04167264cb3a58aaad53e6a
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-07-18 12:11:53 -0300

    feat: implementa tela de pesquisa com SearchBar e listagem dinâmica de filmes
    - Criada tela Search com integração da SearchBar e MovieCard
    - Adicionado debounce para requisições com base na query do usuário
    - Utilizado hook useFetch para buscar filmes da API com controle de loading e erro
    - Implementado layout responsivo com FlatList em colunas
    - Adicionadas mensagens dinâmicas e estados de vazio para melhorar experiência do usuário
    - Ajustado componente SearchBar para aceitar props: value e onTextChange

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,15 +4,18 @@ import {icons} from "@/constants/icons";
 interface Props {
     placeholder: string;
     onPress?: () => void;
+    onChangeText?: (text:string) => void;
+    value?: string;
 }
 
-const SearchBar = ({placeholder, onPress}: Props) => {
+const SearchBar = ({placeholder, onPress, value, onChangeText}: Props) => {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
             <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#AB8BFF" />
             <TextInput onPress={onPress}
+                       value={value}
                        placeholder={placeholder}
-                       onChangeText={()=>{}}
+                       onChangeText={onChangeText}
                        placeholderTextColor="#A8B5DB"
                        className="flex-1 ml-2 text-white" />
         </View>
